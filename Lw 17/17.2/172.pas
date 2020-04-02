ad3d70b72352d49d557499f6b3d568ca6b4f39ac
unknown <nikolai-4545@mail.ru>
2020-04-02 17:19:53 +0300

    Исправил 17.2 и 17.3

--- Lw 17/17.2/172.pas
+++ Lw 17/17.2/172.pas
@@ -1,37 +1,15 @@
 PROGRAM TestOfReadNumber(INPUT, OUTPUT);
 VAR
-  N, MaxInt: INTEGER;
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
-VAR
-  Ch: CHAR;
-BEGIN {ReadDigit}
-  D := -1;
-  IF (NOT EOLN(F)) 
-  THEN
-    BEGIN
-      READ(F, Ch);
-      IF (Ch = '0') THEN D := 0 ELSE
-      IF (Ch = '1') THEN D := 1 ELSE
-      IF (Ch = '2') THEN D := 2 ELSE
-      IF (Ch = '3') THEN D := 3 ELSE
-      IF (Ch = '4') THEN D := 4 ELSE
-      IF (Ch = '5') THEN D := 5 ELSE
-      IF (Ch = '6') THEN D := 6 ELSE
-      IF (Ch = '7') THEN D := 7 ELSE
-      IF (Ch = '8') THEN D := 8 ELSE
-      IF (Ch = '9') THEN D := 9
-    END;
-END; {ReadDigit}
-
+  N: INTEGER;
 PROCEDURE ReadDigitOrMinus(VAR F: TEXT; VAR D: INTEGER; VAR Minus: BOOLEAN);
 VAR
   Ch: CHAR;
-BEGIN {ReadDigit}
+BEGIN {ReadDigitOrMinus}
   D := -1;
-  Minus := FALSE;
   IF (NOT EOLN(F)) 
   THEN
     BEGIN
+      Minus := FALSE;
       READ(F, Ch);
       IF (Ch = '0') THEN D := 0 ELSE
       IF (Ch = '1') THEN D := 1 ELSE
@@ -45,13 +23,14 @@ BEGIN {ReadDigit}
       IF (Ch = '9') THEN D := 9 ELSE
       IF (Ch = '-') THEN Minus := TRUE
     END
-END; {ReadDigit}
+END; {ReadDigitOrMinus}
 
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER; VAR MaxInt: INTEGER);
+PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);
 VAR
   D: INTEGER;
   OverFlow: BOOLEAN;
   Minus: BOOLEAN;
+  Void: BOOLEAN;
 BEGIN {ReadNumber}
   OverFlow := FALSE;
   N := 0;
@@ -59,18 +38,18 @@ BEGIN {ReadNumber}
   IF Minus
   THEN
     D := 0;
-  WHILE (D <> -1) AND NOT(OverFlow)
+  WHILE (D <> -1) AND NOT (OverFlow)
   DO
     BEGIN
-      IF (N * 10 + D <= MaxInt)
+      IF (N * 10 + D > MAXINT) OR (N * 10 + D < 0) OR (N > (MAXINT DIV 10) +1)
       THEN
-        N := N * 10 + D
-      ELSE
         BEGIN
           Overflow := TRUE;
           N := -1;
-        END;
-      ReadDigit(INPUT, D)
+        END
+      ELSE
+        N := N * 10 + D;
+      ReadDigitOrminus(INPUT, D, Void)
     END;
   IF Minus AND NOT (OverFlow)
   THEN
@@ -78,7 +57,6 @@ BEGIN {ReadNumber}
 END; {ReadNumber}
   
 BEGIN {TestOfReadNumber}
-  Maxint := 10000;
-  ReadNumber(INPUT, N, MaxInt);
+  ReadNumber(INPUT, N);
   WRITELN(N)
 END. {TestOfReadNumber}
--- Lw 17/17.3/173.pas
+++ Lw 17/17.3/173.pas
@@ -1,39 +1,17 @@
 PROGRAM Stat(INPUT, OUTPUT);
 VAR
-  N, Min, Max, Sum, Counter, D, MaxInt: INTEGER;
+  N, Min, Max, Sum, Counter, D: INTEGER;
   Ch: CHAR;
   OverFlow: BOOLEAN;
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
-VAR
-  Ch: CHAR;
-BEGIN {ReadDigit}
-  D := -1;
-  IF (NOT EOLN(F)) 
-  THEN
-    BEGIN
-      READ(F, Ch);
-      IF (Ch = '0') THEN D := 0 ELSE
-      IF (Ch = '1') THEN D := 1 ELSE
-      IF (Ch = '2') THEN D := 2 ELSE
-      IF (Ch = '3') THEN D := 3 ELSE
-      IF (Ch = '4') THEN D := 4 ELSE
-      IF (Ch = '5') THEN D := 5 ELSE
-      IF (Ch = '6') THEN D := 6 ELSE
-      IF (Ch = '7') THEN D := 7 ELSE
-      IF (Ch = '8') THEN D := 8 ELSE
-      IF (Ch = '9') THEN D := 9
-    END;
-END; {ReadDigit}
-
 PROCEDURE ReadDigitOrMinus(VAR F: TEXT; VAR D: INTEGER; VAR Minus: BOOLEAN);
 VAR
   Ch: CHAR;
-BEGIN {ReadDigit}
+BEGIN {ReadDigitOrMinus}
   D := -1;
-  Minus := FALSE;
   IF (NOT EOLN(F)) 
   THEN
     BEGIN
+      Minus := FALSE;
       READ(F, Ch);
       IF (Ch = '0') THEN D := 0 ELSE
       IF (Ch = '1') THEN D := 1 ELSE
@@ -46,14 +24,15 @@ BEGIN {ReadDigit}
       IF (Ch = '8') THEN D := 8 ELSE
       IF (Ch = '9') THEN D := 9 ELSE
       IF (Ch = '-') THEN Minus := TRUE
-    END;
-END; {ReadDigit}
+    END
+END; {ReadDigitOrMinus}
 
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER; VAR MaxInt: INTEGER);
-VAR 
+PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);
+VAR
   D: INTEGER;
   OverFlow: BOOLEAN;
   Minus: BOOLEAN;
+  Void: BOOLEAN;
 BEGIN {ReadNumber}
   OverFlow := FALSE;
   N := 0;
@@ -61,18 +40,18 @@ BEGIN {ReadNumber}
   IF Minus
   THEN
     D := 0;
-  WHILE (D <> -1) AND NOT(OverFlow)
+  WHILE (D <> -1) AND NOT (OverFlow)
   DO
     BEGIN
-      IF (N * 10 + D <= MaxInt)
+      IF (N * 10 + D > MAXINT) OR (N * 10 + D < 0) OR (N > (MAXINT DIV 10) +1)
       THEN
-        N := N * 10 + D
-      ELSE
         BEGIN
           Overflow := TRUE;
           N := -1;
-        END;
-      ReadDigit(INPUT, D)
+        END
+      ELSE
+        N := N * 10 + D;
+      ReadDigitOrminus(INPUT, D, Void)
     END;
   IF Minus AND NOT (OverFlow)
   THEN
@@ -81,12 +60,11 @@ END; {ReadNumber}
   
 BEGIN {Stat}
   OverFlow := FALSE;
-  MaxInt := 10000;
   WRITELN('Enter numbers with a space, for example:');
   WRITELN('-5 100 0');
   WRITELN('WARNING: Incorrect characters the program takes as 0');
   WRITELN('WARNING: There is a limitation ', MaxInt, ', so value more then ', MaxInt, ' and value less then ', -MaxInt, ' the program takes as -1');
-  ReadNumber(INPUT, N, MaxInt);
+  ReadNumber(INPUT, N);
   WRITE('You entered: ', N);
   Sum := N;
   Counter := 1;
@@ -95,7 +73,7 @@ BEGIN {Stat}
   WHILE (NOT EOLN(INPUT))
   DO
     BEGIN
-      ReadNumber(INPUT, N, MaxInt);
+      ReadNumber(INPUT, N);
       WRITE(' ', N);
       IF (N < Min)
       THEN
@@ -103,7 +81,7 @@ BEGIN {Stat}
       IF (N > Max)
       THEN
         Max := N;
-      IF ((Sum + N) > MaxInt) OR ((Sum + N) < -MaxInt)
+      IF (Sum + N > MAXINT) OR (Sum + N < -MAXINT) OR ((Sum > 0) AND (N > 0) AND (Sum + N < 0)) OR ((Sum < 0) AND (N < 0) AND (Sum + N > 0))
       THEN
         OverFlow := TRUE
       ELSE
