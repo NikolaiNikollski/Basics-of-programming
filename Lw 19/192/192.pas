14790d22d263fb2e2bd62ad541b2fbeaf6d8f763
unknown <nikolai-4545@mail.ru>
2020-04-15 14:13:12 +0300

    Lw 19

--- Lw 19/192/192.pas
+++ Lw 19/192/192.pas
@@ -6,8 +6,7 @@ VAR
   Copying: BOOLEAN;
   D, VarDate: Date;
   TFile, DateFile: FileOfDate;
-  FInput: TEXT;
- 
+  FInput: TEXT; 
 BEGIN{SortDate}
   ASSIGN(DateFile, 'DF.DAT');
   ASSIGN(TFile, 'TF.DAT');
@@ -18,8 +17,8 @@ BEGIN{SortDate}
   READLN(FInput);
   Write(DateFile, VarDate);
   RESET(DateFile);
-  WHILE NOT EOF(FInput)
-  DO
+  WHILE NOT EOF(FInput) 
+  DO  
     BEGIN {ѕоместить новую дату в DateFile в соответствующее место}
       ReadDate(FInput, D);
       READLN(FInput);
@@ -33,20 +32,41 @@ BEGIN{SortDate}
             DO
               BEGIN
                 READ(DateFile, VarDate);
-                IF Less(VarDate,D)
+                IF Less(VarDate, D)
                 THEN
                   WRITE(TFile, VarDate)
                 ELSE
                   Copying := FALSE
               END
           END;  
-          {копируем D в TFile}
-          WRITE(TFile, D);
-          {копируем остаток DateFile в TFile}
-          {копируем TFile в DateFile}
-        END;
+          WRITE(TFile, D); {копируем D в TFile}
+          BEGIN {копируем остаток DateFile в TFile}
+            IF NOT Copying
+            THEN
+              WRITE(TFile, VarDate);
+            WHILE NOT EOF(DateFile)
+            DO
+              BEGIN
+                READ(DateFile, VarDate);
+                WRITE(TFile, VarDate)
+              END 
+          END; 
+          BEGIN {копируем TFile в DateFile}
+            RESET(TFile);
+            REWRITE(DateFile); 
+            WHILE NOT EOF(TFile)
+            DO
+              BEGIN
+                READ(TFile, VarDate);
+                WRITE(DateFile, VarDate)
+              END;
+            RESET(DateFile)
+          END
+        END
     END;
-
-  { опируем DateFile в OUTPUT}
+  BEGIN { опируем DateFile в OUTPUT}
+    RESET(DateFile);
+    CopyOut(DateFile);
+  END
 END.{SortDate}
  
--- Lw 19/192/Module_192.pas
+++ Lw 19/192/Module_192.pas
@@ -4,42 +4,89 @@ INTERFACE
   TYPE
     Month = (NoMonth, Jan, Feb, Mar, Apr, May, Jun,
                     Jul, Aug, Sep, Oct, Nov, Dec);
-    DayNum = 1 .. 31;
+    DayNum = 0 .. 100000; {Verification in Procedure ReadDate}
     Date = RECORD
              Mo : Month;
              Day: DayNum
            END;
+    FileOfDate = FILE OF Date;
   PROCEDURE ReadDate(VAR FIn: TEXT; VAR Result: Date);
   PROCEDURE ReadMonth(VAR FIn: TEXT; VAR Mo: Month);
- { PROCEDURE WriteDate(VAR FOut: TEXT; VAR Result: Date);
-  PROCEDURE WriteMonth(VAR FOut: TEXT; VAR Mo: Month); }
+  FUNCTION Less(VAR D1, D2: Date): BOOLEAN;
+  PROCEDURE CopyOut(VAR DateFile: FileOfDate);
+  PROCEDURE WriteDate(VAR FOut: TEXT; VAR Result: Date);
+  PROCEDURE WriteMonth(VAR FOut: TEXT; VAR Mo: Month);
+
 IMPLEMENTATION
- 
   PROCEDURE ReadDate(VAR FIn: TEXT; VAR Result: Date);
     BEGIN {ReadDate}
       ReadMonth(FIn, Result.Mo);
-      READ(FIn, Result.Day) 
+      IF NOT EOLN(FIn)
+      THEN
+        READ(FIn, Result.Day);
+      IF Result.Day > 31
+      THEN
+        Result.Day := 0;
     END; {ReadDate}
 
   PROCEDURE ReadMonth(VAR FIn: TEXT; VAR Mo: Month);
   VAR
-    Ch1, Ch2, Ch3: CHAR;  
+    Ch1, Ch2, Ch3 : CHAR;
   BEGIN{ReadMonth}
-    READ(FIn, Ch1, Ch2, Ch3);
-    IF (Ch1 = 'J') AND (Ch2 = 'A') AND (Ch3 = 'N') THEN Mo := Jan ELSE
-    IF (Ch1 = 'F') AND (Ch2 = 'E') AND (Ch3 = 'B') THEN Mo := Feb ELSE
-    IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'R') THEN Mo := Mar ELSE
-    IF (Ch1 = 'A') AND (Ch2 = 'P') AND (Ch3 = 'R') THEN Mo := Apr ELSE
-    IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'Y') THEN Mo := May ELSE
-    IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'N') THEN Mo := Jun ELSE
-    IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'L') THEN Mo := Jul ELSE
-    IF (Ch1 = 'A') AND (Ch2 = 'U') AND (Ch3 = 'G') THEN Mo := Aug ELSE
-    IF (Ch1 = 'S') AND (Ch2 = 'E') AND (Ch3 = 'P') THEN Mo := Sep ELSE
-    IF (Ch1 = 'O') AND (Ch2 = 'C') AND (Ch3 = 'T') THEN Mo := Oct ELSE
-    IF (Ch1 = 'N') AND (Ch2 = 'O') AND (Ch3 = 'V') THEN Mo := Nov ELSE
-    IF (Ch1 = 'D') AND (Ch2 = 'E') AND (Ch3 = 'C') THEN Mo := Dec
+    IF NOT EOLN(FIn)
+    THEN
+      READ(FIn, Ch1);
+    IF NOT EOLN(FIn)
+    THEN
+      READ(FIn, Ch2)
+    ELSE Mo := NoMonth;
+    IF NOT EOLN(FIn)
+    THEN
+      BEGIN
+        READ(FIn, Ch3);
+        IF (Ch1 = 'J') AND (Ch2 = 'A') AND (Ch3 = 'N') THEN Mo := JAN ELSE
+        IF (Ch1 = 'F') AND (Ch2 = 'E') AND (Ch3 = 'B') THEN Mo := FEB ELSE
+        IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'R') THEN Mo := MAR ELSE
+        IF (Ch1 = 'A') AND (Ch2 = 'P') AND (Ch3 = 'R') THEN Mo := APR ELSE
+        IF (Ch1 = 'M') AND (Ch2 = 'A') AND (Ch3 = 'Y') THEN Mo := MAY ELSE
+        IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'N') THEN Mo := JUN ELSE
+        IF (Ch1 = 'J') AND (Ch2 = 'U') AND (Ch3 = 'L') THEN Mo := JUL ELSE
+        IF (Ch1 = 'A') AND (Ch2 = 'U') AND (Ch3 = 'G') THEN Mo := AUG ELSE
+        IF (Ch1 = 'S') AND (Ch2 = 'E') AND (Ch3 = 'P') THEN Mo := SEP ELSE
+        IF (Ch1 = 'O') AND (Ch2 = 'C') AND (Ch3 = 'T') THEN Mo := OCT ELSE
+        IF (Ch1 = 'N') AND (Ch2 = 'O') AND (Ch3 = 'V') THEN Mo := NOV ELSE
+        IF (Ch1 = 'D') AND (Ch2 = 'E') AND (Ch3 = 'C') THEN Mo := DEC
+        ELSE Mo := NoMonth
+      END
     ELSE Mo := NoMonth
   END;{ReadMonth}
+
+  FUNCTION Less(VAR D1, D2: Date): BOOLEAN;
+  {Less:= D1 < D2}
+  BEGIN {Less}
+    IF D1.Mo < D2.Mo
+    THEN
+      Less := TRUE
+    ELSE
+      IF D1.Mo > D2.Mo
+        THEN
+          Less := FALSE
+        ELSE {D1.Mo = D2.Mo}
+          Less := (D1.Day < D2.Day)
+  END; {Less}
+
+  PROCEDURE CopyOut(VAR DateFile: FileOfDate);
+  VAR
+    VarDate: Date;
+  BEGIN {CopyOut}
+    WHILE NOT EOF(DateFile)
+    DO
+      BEGIN
+        READ(DateFile, VarDate);
+        WriteDate(OUTPUT, VarDate);
+        WRITELN;
+      END
+  END;{CopyOut}
 
   PROCEDURE WriteDate(VAR FOut: TEXT; VAR Result: Date);
   BEGIN{WriteDate}
@@ -65,12 +112,10 @@ IMPLEMENTATION
     IF Mo = Dec THEN WRITE('DEC')
     ELSE WRITE('NoMonth')
   END; {WriteMonth}
-
-
 BEGIN
 END.
 
 
 
-  
+
 
